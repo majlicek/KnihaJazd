2aa3535b48342710bfc959dc6c69830c1a66af29
Matej <Matej@MatejPerejda>
2014-12-14 00:43:11 +0100

    GUI_v14

---
+++ Cestak/src/main/java/sk/upjs/ics/cestak/DtbAutoDAO.java
@@ -0,0 +1,26 @@
+package sk.upjs.ics.cestak;
+
+import java.util.List;
+
+/**
+ *
+ * @author Matej Perejda
+ */
+public class DtbAutoDAO implements AutoDAO {
+
+    @Override
+    public List<Auto> zoznamPodlaPouzivatela(Login login) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void saveAuto(Auto auto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void vymazAuto(Auto auto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
